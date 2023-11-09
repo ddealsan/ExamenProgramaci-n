9063d48e0058a987d703c9f7b2b50f2601a1a47f
ddealsan <ddealsan@myuax.com>
2023-11-09 11:53:27 +0100

    creamos y definimos la clase estudiante

--- Gestion de estudiantes.cpp
+++ Gestion de estudiantes.cpp
@@ -4,3 +4,21 @@
 
 using namespace std;
 
+class Estudiante {
+    private:
+        string nombre;
+        int edad;
+        string grado;
+    public:
+        Estudiante(string, int, string, string);
+        void mostrar_info();
+};
+
+Estudiante::Estudiante(string _nombre, int _edad, string _grado, string _seccion){
+    nombre = _nombre;
+    edad = _edad;
+    grado = _grado;
+}
+
+
+
