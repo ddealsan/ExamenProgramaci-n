ef19b46e29267cc15b03f60bc030894e9d87f981
ddealsan <ddealsan@myuax.com>
2023-11-09 12:55:58 +0100

    Copiamos el anterior codigo del apartado gestion estiudiantes y lo modificamos añandiendo las asignaturas

---
+++ Registro de materias.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Estudiante {//cramos la clase estudiante
+private:
+    string nombre;//definimos lo que hay dentro de ella
+    int edad;
+    string grado;
+    vector<string> materias;
+public:
+    Estudiante(string, int, string);
+    void registrar_materia(const string&);
+    void mostrar_info();
+    void mostrar_materias();
+};
+
+Estudiante::Estudiante(string _nombre, int _edad, string _grado){
+    nombre = _nombre;
+    edad = _edad;
+    grado = _grado;
+}
+
+void Estudiante::registrar_materia(const string& materia) {
+    materias.push_back(materia);
+}
+
+void Estudiante::mostrar_info(){//definimos el mostrar informacion
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Grado: " << grado << endl;
+}
+
+void Estudiante::mostrar_materias() {
+    if (materias.empty()) {
+        cout << "El estudiante no tiene materieas registradas." << endl;
+    } else {
+        cout << "Materiasf registradas:" << endl;
+        for (const string& materia : materias) {
+            cout << "- " << materia << endl;
+        }
+    }
+}
+
+
+int main(){//definimos el main
+    Estudiante estudiante1("Juan", 15, "9");
+    cout<<"\n";
+    Estudiante estudiante2("Maria", 16, "10");//creamos los estudiantes
+
+    estudiante1.mostrar_info();//mostramos la informacion
+    estudiante2.mostrar_info();
+
+    return 0;
+}
