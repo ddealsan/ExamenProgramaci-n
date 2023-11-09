5e04d1616b4ce439e3e75012e3b743f7ff883c28
ddealsan <ddealsan@myuax.com>
2023-11-09 13:47:38 +0100

    Creamos las clases necesarias

--- Apartado9/Gestión de Excepciones Personalizadas.cpp
+++ Apartado9/Gestión de Excepciones Personalizadas.cpp
@@ -4,3 +4,41 @@
 #include <stdexcept>
 
 using namespace std;
+
+class MateriaYaRegistradaException : public exception {
+public:
+    const char* what() const throw() {
+        return "Error: La materia ya ha sido registrada.";
+    }
+};
+
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+    string grado;
+    vector<string> materias;
+
+public:
+    Estudiante(string _nombre, int _edad, string _grado) : nombre(_nombre), edad(_edad), grado(_grado) {}
+
+    string obtenerGrado() const {
+        return grado;
+    }
+
+    void registrarMateria(string materia) {
+        for (const string& materiaRegistrada : materias) {
+            if (materiaRegistrada == materia) {
+                throw MateriaYaRegistradaException();
+            }
+        }
+        materias.push_back(materia);
+    }
+
+    void mostrarMaterias() const {
+        cout << "Materias registradas por " << nombre << ":" << endl;
+        for (const string& materia : materias) {
+            cout << "- " << materia << endl;
+        }
+    }
+};
