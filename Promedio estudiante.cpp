581078ab9d1d1dca12fc09078c78832691e59213
ddealsan <ddealsan@myuax.com>
2023-11-09 13:07:48 +0100

    Definimos el main y lo rellenamos

--- Promedio estudiante.cpp
+++ Promedio estudiante.cpp
@@ -3,3 +3,36 @@
 
 using namespace std;
 
+double calcularPromedio(const std::vector<int>& calificaciones) {//definimos la funcion
+    int suma = 0;
+    for (int calificacion : calificaciones) {
+        suma += calificacion;
+    }
+
+    double promedio = static_cast<double>(suma) / calificaciones.size();
+    return promedio;
+
+}
+
+
+int main(){
+    cout << "Ingrese las calificaciones (ingrese -1 para finalizar):\n";
+
+    vector<int> calificaciones;
+    int calificacion;
+
+    while (true) {
+        cin >> calificacion;
+
+        if (calificacion == -1) {
+            break;  // El usuario ingresó -1 para finalizar
+        }
+
+        calificaciones.push_back(calificacion);
+    }
+
+    double promedio = calcularPromedio(calificaciones);
+    cout << "El promedio de las calificaciones es: " << promedio << endl;//
+
+    return 0;
+}
