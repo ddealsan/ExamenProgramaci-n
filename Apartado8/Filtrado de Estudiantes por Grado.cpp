242f5be213c2ff991dbe14782ea657abf0e66ab8
ddealsan <ddealsan@myuax.com>
2023-11-09 13:28:58 +0100

    Creamos la clase estudiante

--- Apartado8/Filtrado de Estudiantes por Grado.cpp
+++ Apartado8/Filtrado de Estudiantes por Grado.cpp
@@ -4,3 +4,19 @@
 
 using namespace std;
 
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+    string grado;
+
+public:
+    Estudiante(string _nombre, int _edad, string _grado) : nombre(_nombre), edad(_edad), grado(_grado) {}
+    string obtenerGrado() const {
+        return grado;
+    }
+
+    void mostrar_info() {
+        cout << "Nombre: " << nombre << ", Edad: " << edad << ", Grado: " << grado << endl;
+    }
+};
