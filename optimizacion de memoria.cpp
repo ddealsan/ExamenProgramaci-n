799ae2098aa6067201a06c6ac9525151cbde0773
ddealsan <ddealsan@myuax.com>
2023-11-09 12:15:04 +0100

    definimos en el intercambio la funcion intercambui y intercambiamos las variables

--- optimizacion de memoria.cpp
+++ optimizacion de memoria.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void intercambio(int *a, int *b){
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
 
+int main(){
+int A = 10, B = 20;
+cout << "valores origniales de A y B: " << A << " " << B << endl;
+int*a = &A;
+int*b = &B;
+intercambio(a, b);
 }
