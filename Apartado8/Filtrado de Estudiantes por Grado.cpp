71e9e5788be33e81e02681cc411c9716cbdb973f
ddealsan <ddealsan@myuax.com>
2023-11-09 13:43:37 +0100

    Definimos y añadimos el Main haciendo que el usuario pueda elegir el grado

--- Apartado8/Filtrado de Estudiantes por Grado.cpp
+++ Apartado8/Filtrado de Estudiantes por Grado.cpp
@@ -12,22 +12,44 @@ private:
 
 public:
     Estudiante(string _nombre, int _edad, string _grado) : nombre(_nombre), edad(_edad), grado(_grado) {}
+
     string obtenerGrado() const {
         return grado;
     }
 
-    void mostrar_info() {
+    void mostrar_info() const {
         cout << "Nombre: " << nombre << ", Edad: " << edad << ", Grado: " << grado << endl;
     }
 };
 
 vector<Estudiante> filtrarEstudiantesPorGrado(const vector<Estudiante>& estudiantes, const string& grado) {
     vector<Estudiante> estudiantesFiltrados;
+
     for (const Estudiante& estudiante : estudiantes) {
         if (estudiante.obtenerGrado() == grado) {
             estudiantesFiltrados.push_back(estudiante);
         }
     }
+
     return estudiantesFiltrados;
 }
 
+int main() {
+    Estudiante estudiante1("Juan", 15, "9A");
+    Estudiante estudiante2("Maria", 16, "10B");
+    Estudiante estudiante3("Carlos", 14, "9A");
+    Estudiante estudiante4("Laura", 15, "10B");
+
+    vector<Estudiante> listaEstudiantes = {estudiante1, estudiante2, estudiante3, estudiante4};
+    cout <<"que grado quieres que sea, 9A o 10B?"<<endl;
+    string gradoFiltrar;
+    cin>>gradoFiltrar;
+    vector<Estudiante> estudiantesFiltrados = filtrarEstudiantesPorGrado(listaEstudiantes, gradoFiltrar);
+
+    cout << "Estudiantes del grado " << gradoFiltrar << ":" << endl;
+    for (const Estudiante& estudiante : estudiantesFiltrados) {
+        estudiante.mostrar_info();
+    }
+
+    return 0;
+}
