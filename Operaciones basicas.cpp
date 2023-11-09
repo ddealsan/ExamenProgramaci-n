79ac1a0b7086d82b405fb3d08a9a9e8e70ae2aed
ddealsan <ddealsan@myuax.com>
2023-11-09 11:44:10 +0100

    Añadimos anotaciones para explicar para que es cada cosa

--- Operaciones basicas.cpp
+++ Operaciones basicas.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main(){
 
-    int a, b;
-    cout << "Ingrese el primer numero: "; cin >> a;
+    int a, b; //definimos las variables
+    cout << "Ingrese el primer numero: "; cin >> a; //pedimos al usuario que ingrese los numeros
     cout << "Ingrese el segundo numero: "; cin >> b;
-    int suma = a + b;
-    cout << "La suma es: " << suma << endl;
+
+    int suma = a + b;//realizamos la suma
+
+    cout << "La suma es: " << suma << endl; //imprimimos el resultado
+
     return 0;
 }
