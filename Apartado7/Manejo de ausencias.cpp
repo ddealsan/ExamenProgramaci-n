79e4854ca12735fde397a6c3be37cfce4688b876
ddealsan <ddealsan@myuax.com>
2023-11-09 13:17:06 +0100

    añadimos anotaciones

--- Apartado7/Manejo de ausencias.cpp
+++ Apartado7/Manejo de ausencias.cpp
@@ -9,19 +9,19 @@ private:
     string estado;
 
 public:
-    RegistroAsistencia(string _fecha, string _estado) : fecha(_fecha), estado(_estado) {}
+    RegistroAsistencia(string _fecha, string _estado) : fecha(_fecha), estado(_estado) {}//definimos el constructor
 
     void mostrar_asistencia() {
-        cout << "Fecha de la clase: " << fecha << endl;
+        cout << "Fecha de la clase: " << fecha << endl;//mostramos la informacion
         cout << "Estado de asistencia: " << estado << endl;
     }
 };
 
 int main(){
     RegistroAsistencia asistencia1("12/03/2021", "Presente");
-    RegistroAsistencia asistencia2("13/03/2021", "Ausente");
+    RegistroAsistencia asistencia2("13/03/2021", "Ausente");//creamos los registros de asistencia
 
-    asistencia1.mostrar_asistencia();
+    asistencia1.mostrar_asistencia();//mostramos la informacion
     asistencia2.mostrar_asistencia();
 
     return 0;}
