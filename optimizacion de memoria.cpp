4af62cb2b94f989983fa098e86a1a9868e3773bd
ddealsan <ddealsan@myuax.com>
2023-11-09 12:17:39 +0100

    Añadimos las anotaciones

--- optimizacion de memoria.cpp
+++ optimizacion de memoria.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void intercambio(int *a, int *b){
+void intercambio(int *a, int *b){//definimos la funcion
     int aux = *a;
-    *a = *b;
+    *a = *b;//intercambiamos los valores
     *b = aux;
 }
 
 int main(){
 int A = 10, B = 20;
-cout << "valores origniales de A y B: " << A << " " << B << endl;
+cout << "valores origniales de A y B: " << A << " " << B << endl;//imprimimos los valores originales
 int*a = &A;
-int*b = &B;
-intercambio(a, b);
-cout << "valores intercambiados de A y B: " << A << " " << B << endl;
+int*b = &B;//definimos los punteros
+intercambio(a, b);//llamamos a la funcion para intercambiar los valores
+cout << "valores intercambiados de A y B: " << A << " " << B << endl;//imprimimos los valores intercambiados
 return 0;
 }
